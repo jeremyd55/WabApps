905939bf6dd00d032be53daab4342c73164014d5
jeanmichel <collajm@hotmail.com>
2018-07-04 12:34:33 +0200

    Autoloader, récupération de l'environment

---
+++ Bin/Core/Autoload.php
@@ -0,0 +1,73 @@
+<?php namespace Framework;
+
+
+class Autoload
+{
+
+    private $environment = null;
+
+    public function load(array $pathEnv = array())
+    {
+        $this->environment = $this->setEnvironment(Environment::class, $pathEnv);
+        $this->register();
+    }
+
+    private function setEnvironment($env, $path)
+    {
+        $class = explode('\\', $env);
+        $class = str_replace('\\', DIRECTORY_SEPARATOR, __DIR__) . DIRECTORY_SEPARATOR
+            .$class[count($class)-1] . '.php';
+        if($this->requireFiles($class)) {
+            if(class_exists($env)) {
+                return new $env($path);
+            }
+        }
+    }
+
+    private function register()
+    {
+        spl_autoload_register(array($this, 'loadClass'), true);
+    }
+
+    private function loadClass($class)
+    {
+        $prefix = $class;
+        while(false !== $pos = strrpos($prefix, '\\')) {
+
+            $prefix = substr($class, 0, $pos + 1);
+            $relative = substr($class, $pos + 1);
+
+            $file = $this->fileLoad($prefix, $relative);
+
+            if($file)
+                return $file;
+
+            $prefix = rtrim($prefix, '\\');
+        }
+        return false;
+    }
+
+    private function fileLoad($prefix, $relative)
+    {
+        $prefix = substr($prefix, 0, -1);
+        if($prefix !== __NAMESPACE__) {
+            return false;
+        }
+
+        $file = str_replace('\\', DIRECTORY_SEPARATOR, __DIR__) . DIRECTORY_SEPARATOR
+            .str_replace('\\', DIRECTORY_SEPARATOR, $relative) . '.php';
+        if($this->requireFiles($file))
+            return $file;
+
+        return false;
+    }
+
+    private function requireFiles(string $file): bool
+    {
+        if (file_exists($file)) {
+            require $file;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Bin/Core/Environment.php
@@ -0,0 +1,56 @@
+<?php namespace Framework;
+
+
+final class Environment
+{
+
+    const SEPARATOR = "=";
+    const SEPARATOR_OPTION = ",";
+
+    public function __construct(array $paths = array())
+    {
+        foreach($paths as $path) {
+            if((! is_readable($path)) && is_dir($path)) {
+                throw new \RuntimeException("Not loaded $path !");
+            }
+            $this->setEnvironment($this->parser(file_get_contents($path)));
+        }
+    }
+
+    private function setEnvironment(array $data): void
+    {
+        foreach($data as $key => $value) {
+            if(isset($_ENV[$key]) || isset($_SERVER[$key])) {
+                continue;
+            }
+            $_ENV[$key] = $value;
+            $envKey[$key] = true;
+        }
+        if($envKey) {
+            $envKey = implode(',', array_keys($envKey));
+            $_ENV['FRAMEWORK'] = $envKey;
+        }
+    }
+
+    private function parser(?string $data): array
+    {
+        $data = $this->cleanParser($data);
+        foreach(explode('\\n', $data) as $val) {
+            if(!empty(preg_match('/(^[\w\s=:\/@.,-]+)/m', $val, $match))) {
+                if(is_bool(strpos($match[0], self::SEPARATOR)) === false) {
+                    list($key, $value) = explode(self::SEPARATOR, $match[0]);
+                    $environment[trim($key)] = (is_bool(strpos($value, self::SEPARATOR_OPTION)) === false)
+                        ? explode(self::SEPARATOR_OPTION, $value)
+                        : trim($value);
+                }
+            }
+        }
+        return $environment ?? [];
+    }
+
+    private function cleanParser(?string $value): string
+    {
+        $value = str_replace(array("\r\n", "\r"), "\\n", $value);
+        return $value;
+    }
+}
